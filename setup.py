f012f275c18c7b44309505f1b9924f897f52495c
Bradley Wright <brad@intranation.com>
2011-12-05 22:44:30 +0000

    Bare bones setup tools script

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='html5writer',
+    version='0.1',
+    author='Bradley Wright <brad@intranation.com>',
+    packages=['html5writer'],
+    description='',
+    long_description='',
+    license='BSD License',
+    include_package_data=True,
+    install_requires=['docutils']
+)
