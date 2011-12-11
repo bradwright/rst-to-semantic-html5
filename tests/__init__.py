bffa533988bdfbfa55aedcc2d58d450cd535403b
Bradley Wright <brad@intranation.com>
2011-12-11 12:29:14 +0000

    Get tests running

---
+++ tests/__init__.py
@@ -0,0 +1,16 @@
+import unittest
+from html5writer.html5writer import SemanticHTML5Writer
+from docutils.core import publish_parts
+
+def text_to_rst(text):
+    parts = publish_parts(writer=SemanticHTML5Writer(), source=text)
+    return parts['fragment']
+
+class TestNoDivSection(unittest.TestCase):
+    def test_div(self):
+        in_text = """Lorem ipsem dolor sit amet"""
+        out_text = u"""<p>Lorem ipsem dolor sit amet</p>\n"""
+        self.assertEqual(text_to_rst(in_text), out_text)
+
+if __name__ == '__main__':
+    unittest.main()
