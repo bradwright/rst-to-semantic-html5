9faf553084e82b496881a03f7eb900c9d09ce46d
Bradley Wright <brad@intranation.com>
2011-12-11 10:27:09 +0000

    Add tests command and module to setup.py (empty right now)

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
+import os
 from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='html5writer',
     version='0.1',
     author='Bradley Wright <brad@intranation.com>',
-    packages=['html5writer'],
-    description='',
-    long_description='',
+    packages=['html5writer', 'tests'],
+    keywords = "rst html5 doctutils",
+    description='A REStructured Text writer for writing semantic HTML5',
+    long_description=read('README.rst'),
     license='BSD License',
     include_package_data=True,
     install_requires=['docutils']
