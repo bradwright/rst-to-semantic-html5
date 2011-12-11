95ea7ef653766483420997197e4dabec0621d9aa
Bradley Wright <brad@intranation.com>
2011-12-11 12:30:55 +0000

    Strip trailing whitespace as a test expectation

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,12 +4,12 @@ from docutils.core import publish_parts
 
 def text_to_rst(text):
     parts = publish_parts(writer=SemanticHTML5Writer(), source=text)
-    return parts['fragment']
+    return parts['fragment'].strip()
 
 class TestNoDivSection(unittest.TestCase):
     def test_div(self):
         in_text = """Lorem ipsem dolor sit amet"""
-        out_text = u"""<p>Lorem ipsem dolor sit amet</p>\n"""
+        out_text = u"""<p>Lorem ipsem dolor sit amet</p>"""
         self.assertEqual(text_to_rst(in_text), out_text)
 
 if __name__ == '__main__':
